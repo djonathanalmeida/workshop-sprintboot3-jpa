663e269ce8b20e4281d4cdfb9ac175f9fe1fe5e3
Djonathan Almeida <djonathan_7@hotmail.com>
2023-10-04 13:19:58 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
 
+//CONTROLADOR REST QUE RESPONDE NO CAMINHO "USERS";
+
+// ANOTAÇÃO PARA DIZER QUE ESSA CLASSE É UM RECURSO WEB QUE É IMPLEMENTADO POR UM CONTROLADOR REST
 @RestController
+// ANOTAÇÃO QUE DA O NOME PARA O RECURSO E INDICA QUE O CONTROLADOR IRÁ RECEBER REQUISIÇÕES A URL "www":
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
+	// MÉTODO QUE TEM A FINALIDADE DE SER UM ENDPOINT PRA ACESSAR OS USUÁRIOS:
+	@GetMapping //UTILIZADO PARA INDICAR QUE É UM MÉTODO QUE RESPONDE UMA REQUISIÇÃO DO TIPO GET DO HTTP
+	public ResponseEntity<User> findAll(){ // O RESPONSEENTITY É UM TIPO DO SPRING PARA RETORNAR RESPOSTAS DE REQUISIÇÕES WEB (DENTRO DO <> É O TIPO DA RESPOSTA, NO CASO OS USUÁRIOS)
 		User u = new User(1L, "Maria", "maria@email.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(u);// O "OK" RETORNA A RESPOSTA COM SUCESSO NO HTTP
 	}
 	
 }
