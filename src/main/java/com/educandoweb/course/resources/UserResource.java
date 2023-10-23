056f48a744b7ab76003b48835be39a1372d72b0f
Djonathan Almeida <djonathan_7@hotmail.com>
2023-10-23 20:10:00 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,25 +1,33 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
-//CONTROLADOR REST QUE RESPONDE NO CAMINHO "USERS";
-
-// ANOTAÇÃO PARA DIZER QUE ESSA CLASSE É UM RECURSO WEB QUE É IMPLEMENTADO POR UM CONTROLADOR REST
 @RestController
-// ANOTAÇÃO QUE DA O NOME PARA O RECURSO E INDICA QUE O CONTROLADOR IRÁ RECEBER REQUISIÇÕES A URL "www":
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users/")
 public class UserResource {
-	
-	// MÉTODO QUE TEM A FINALIDADE DE SER UM ENDPOINT PRA ACESSAR OS USUÁRIOS:
-	@GetMapping //UTILIZADO PARA INDICAR QUE É UM MÉTODO QUE RESPONDE UMA REQUISIÇÃO DO TIPO GET DO HTTP
-	public ResponseEntity<User> findAll(){ // O RESPONSEENTITY É UM TIPO DO SPRING PARA RETORNAR RESPOSTAS DE REQUISIÇÕES WEB (DENTRO DO <> É O TIPO DA RESPOSTA, NO CASO OS USUÁRIOS)
-		User u = new User(1L, "Maria", "maria@email.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);// O "OK" RETORNA A RESPOSTA COM SUCESSO NO HTTP
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
